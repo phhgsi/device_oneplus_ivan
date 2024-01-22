79daf2cb94de810e4a5ad6850373ec7cd6f3b35e
bengris32 <bengris32@protonmail.ch>
2024-01-22 07:52:16 +0530

    ivan:Configure gatekeeper stack

--- device.mk
+++ device.mk
@@ -20,6 +20,11 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutils-v32
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
