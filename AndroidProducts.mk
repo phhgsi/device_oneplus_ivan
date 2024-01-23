5a4fb86d430d6dd3b01b5a84fb676aada8e1a8a8
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-23 21:34:18 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ivan.mk
+    $(LOCAL_DIR)/derp_ivan.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ivan-user \
-    lineage_ivan-userdebug \
-    lineage_ivan-eng
+    derp_ivan-user \
+    derp_ivan-userdebug \
+    derp_ivan-eng
