71d4d391d7824f048fa70c74b19e0a325eb017ad
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-24 11:53:43 +0530

    Update and rename derp_ivan.mk to lineage_ivan.mk

--- lineage_ivan.mk
+++ lineage_ivan.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/oneplus/ivan/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := derp_ivan
+PRODUCT_NAME := lineage_ivan
 PRODUCT_DEVICE := ivan
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
